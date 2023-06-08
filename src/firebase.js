b693ada4436ae45dd86555c7d86ecd7699706480
Griffin Quarles <griffinquarles11@gmail.com>
2023-06-08 12:39:28 -0500

    setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZVg3RKzljcYMvsCNtF5YgDZYbzPgm7iQ",
+  authDomain: "rettiwt-9a016.firebaseapp.com",
+  projectId: "rettiwt-9a016",
+  storageBucket: "rettiwt-9a016.appspot.com",
+  messagingSenderId: "242575733792",
+  appId: "1:242575733792:web:2afe38cc355aaa7d2ef845",
+};
+
+const app = initializeApp(firebaseConfig);
